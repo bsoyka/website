4e2cfc5f2650caebb3da3a6334d7522db83d69cc
Ben Soyka <hello@bsoyka.me>
2025-04-20 12:02:46 -0500

    Add brand font to config

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,14 @@
+import presetWebFonts from '@unocss/preset-web-fonts'
 import {defineConfig} from 'unocss'
 
 export default defineConfig({
+    presets:[presetWebFonts({
+        provider: 'bunny', // Privacy-friendly Google Fonts alternative
+        fonts: {
+            // Extends default theme
+            sans: 'Space Grotesk'
+        },
+    })],
     theme: {
         colors: {
             primary: "#507dbc", secondary: "#51b0bd", light: "#f4f9f9", dark: "#04080f"
