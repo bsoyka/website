6b01d7a0843530b092fdf91915344fafe08bc8d9
Ben Soyka <hello@bsoyka.me>
2025-04-20 15:54:17 -0500

    Remove Svelte (unused)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 // @ts-check
 import {defineConfig} from 'astro/config';
 
-import svelte from '@astrojs/svelte';
 import UnoCSS from 'unocss/astro';
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [UnoCSS({
         injectReset: true
-    }),svelte()]
+    })]
 });
