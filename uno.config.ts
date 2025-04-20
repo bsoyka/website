342685e0416dadf118b899247f84bdbfcafc0486
Ben Soyka <hello@bsoyka.me>
2025-04-20 11:19:38 -0500

    Bring in new theme colors

--- uno.config.ts
+++ uno.config.ts
@@ -1,5 +1,9 @@
-import { defineConfig } from 'unocss'
+import {defineConfig} from 'unocss'
 
 export default defineConfig({
-    // ...UnoCSS options
+    theme: {
+        colors: {
+            primary: "#507dbc", secondary: "#51b0bd", light: "#f4f9f9", dark: "#04080f"
+        }
+    }
 })
