80859b11566f22d9fb9982ced5636fc908b7ff51
Ben Soyka <hello@bsoyka.me>
2026-07-18 23:13:57 -0500

    refactor: source all facts from my new Statistician API

--- src/components/FactsCardContent.svelte
+++ src/components/FactsCardContent.svelte
@@ -1,8 +1,8 @@
 <script>
-    export let facts = [];
+    export let facts = null;
 
     let shuffledFacts = [];
-    let currentFacts = ["No facts available."];
+    let currentFacts = [];
 
     function shuffleArray(array) {
         for (let i = array.length - 1; i > 0; i--) {
@@ -22,7 +22,7 @@
     import { onMount } from "svelte";
 
     onMount(() => {
-        if (facts.length > 0) {
+        if (facts && facts.length > 0) {
             shuffleArray(facts);
             shuffledFacts = [...facts];
             currentFacts = shuffledFacts.splice(0, 3); // Set the first 3 facts
@@ -30,15 +30,21 @@
     });
 </script>
 
-<div>
-    <ul class="my-2" id="facts">
-        {#each currentFacts as fact}
-            <li class="max-w p-2 border border-gray-200 rounded-lg">
-                {@html fact}
-            </li>
-        {/each}
-    </ul>
-    <button class="btn-primary" on:click={generateNewFacts}
-        >Generate New Facts</button
-    >
-</div>
+{#if facts === null}
+    <p class="my-2 text-sm text-gray-500">
+        This card is unavailable right now, try again later.
+    </p>
+{:else}
+    <div>
+        <ul class="my-2" id="facts">
+            {#each currentFacts as fact}
+                <li class="max-w p-2 border border-gray-200 rounded-lg">
+                    {@html fact}
+                </li>
+            {/each}
+        </ul>
+        <button class="btn-primary" on:click={generateNewFacts}
+            >Generate New Facts</button
+        >
+    </div>
+{/if}
